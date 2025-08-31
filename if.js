1ba004cdb029fb55b470cd48743546a44614545d
Syed Rasim Ali <alisyedrasim@gmail.com>
2025-08-31 03:47:42 -0700

    Add files via upload

---
+++ if.js
@@ -0,0 +1,11 @@
+var temperature = 35;
+
+if (temperature > 40) {
+  console.log("It's very hot!");
+} else if (temperature > 30) {
+  console.log("It's warm outside.");
+} else if (temperature > 20) {
+  console.log("Nice weather!");
+} else {
+  console.log("It's cold!");
+}
